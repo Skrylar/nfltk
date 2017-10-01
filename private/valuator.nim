c364c2e4eef1cf7d74158d57061a4a465a605186
Joshua Cearley <joshua.cearley@gmail.com>
2017-10-01 17:44:06 -0500

    Value input/output widgets.

--- private/valuator.nim
+++ private/valuator.nim
@@ -2,6 +2,7 @@
 const
   flh = "FL/Fl_Valuator.H"
 
+import enumerations
 import widget
 
 const
@@ -29,4 +30,62 @@ proc format*(self: Valuator; fmt: cstring): cint {.importc: "#.format(@)", heade
 proc round*(self: Valuator; a: cdouble): cdouble {.importc: "#.round(@)", header: flh.}
 proc clamp*(self: Valuator; a: cdouble): cdouble {.importc: "#.clamp(@)", header: flh.}
 proc increment*(self: Valuator; a: cdouble; b: cint): cdouble {.importc: "#.increment(@)", header: flh.}
+
+# Value slider
+const
+  flh_valueslider = "FL/Fl_Value_Slider.H"
+type
+  ValueSliderObj* {.importc: "Fl_Value_Slider", header: flh_valueslider.} = object of ValuatorObj
+  ValueSlider* = ptr ValueSliderObj
+
+proc make_valueslider*(x, y, w, h: cint; text: cstring = nil): ValueSlider {.importcpp: "new Fl_Value_Slider(@)", header: flh_valueslider.}
+
+proc textfont*(self: ValueSlider): Font {.importcpp: "#.textfont(@)", header: flh_valueslider.}
+proc `textfont=`*(self: ValueSlider; s: Font) {.importcpp: "#.textfont(@)", header: flh_valueslider.}
+proc textsize*(self: ValueSlider): Fontsize {.importcpp: "#.textsize(@)", header: flh_valueslider.}
+proc `textsize=`*(self: ValueSlider; s: Fontsize) {.importcpp: "#.textsize(@)", header: flh_valueslider.}
+proc textcolor*(self: ValueSlider): Color {.importcpp: "#.textcolor(@)", header: flh_valueslider.}
+proc `textcolor=`*(self: ValueSlider; s: Color) {.importcpp: "#.textcolor(@)", header: flh_valueslider.}
+
+# Value Output
+const
+  flh_valueoutput = "FL/Fl_Value_Output.H"
+type
+  ValueOutputObj* {.importc: "Fl_Value_Output", header: flh_valueoutput.} = object of ValuatorObj
+  ValueOutput* = ptr ValueOutputObj
+
+proc make_valueoutput*(x, y, w, h: cint; text: cstring = nil): ValueOutput {.importcpp: "new Fl_Value_Output(@)", header: flh_valueoutput.}
+
+proc soft*(self: ValueOutput): cuchar {.importcpp: "#.soft(@)", header: flh_valueoutput.}
+proc `soft=`*(self: ValueOutput; s: cuchar) {.importcpp: "#.soft(@)", header: flh_valueoutput.}
+proc textfont*(self: ValueOutput): Font {.importcpp: "#.textfont(@)", header: flh_valueoutput.}
+proc `textfont=`*(self: ValueOutput; s: Font) {.importcpp: "#.textfont(@)", header: flh_valueoutput.}
+proc textsize*(self: ValueOutput): Fontsize {.importcpp: "#.textsize(@)", header: flh_valueoutput.}
+proc `textsize=`*(self: ValueOutput; s: Fontsize) {.importcpp: "#.textsize(@)", header: flh_valueoutput.}
+proc textcolor*(self: ValueOutput): Color {.importcpp: "#.textcolor(@)", header: flh_valueoutput.}
+proc `textcolor=`*(self: ValueOutput; s: Color) {.importcpp: "#.textcolor(@)", header: flh_valueoutput.}
+
+# Value Input
+const
+  flh_valueinput = "FL/Fl_Value_Input.H"
+type
+  ValueInputObj* {.importc: "Fl_Value_Input", header: flh_valueinput.} = object of ValuatorObj
+  ValueInput* = ptr ValueInputObj
+
+proc make_valueinput*(x, y, w, h: cint; text: cstring = nil): ValueInput {.importcpp: "new Fl_Value_Input(@)", header: flh_valueinput.}
+
+#proc input*(self: ValueInput): Input {.importcpp: "(&#.input)", header: flh_valueinput.}
+proc resize*(x, y, w, h: cint) {.importcpp: "#.resize(@)", header: flh_valueinput.}
+proc shortcut*(): cint {.importcpp: "#.shortcut(@)", header: flh_valueinput.}
+proc `shortcut=`*(s: cint) {.importcpp: "#.shortcut(@)", header: flh_valueinput.}
+proc soft*(): cuchar {.importcpp: "#.soft(@)", header: flh_valueinput.}
+proc `soft=`*(s: cuchar) {.importcpp: "#.soft(@)", header: flh_valueinput.}
+proc textfont*(): Font {.importcpp: "#.textfont(@)", header: flh_valueinput.}
+proc `textfont=`*(s: Font) {.importcpp: "#.textfont(@)", header: flh_valueinput.}
+proc textsize*(): Fontsize {.importcpp: "#.textsize(@)", header: flh_valueinput.}
+proc `textsize=`*(s: Fontsize) {.importcpp: "#.textsize(@)", header: flh_valueinput.}
+proc textcolor*(): Color {.importcpp: "#.textcolor(@)", header: flh_valueinput.}
+proc `textcolor=`*(s: Color) {.importcpp: "#.textcolor(@)", header: flh_valueinput.}
+proc cursor_color*(): Color {.importcpp: "#.cursor_color(@)", header: flh_valueinput.}
+proc `cursor_color=`*(n: Color) {.importcpp: "#.cursor_color(@)", header: flh_valueinput.}
 
